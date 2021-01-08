d50e88c2a3acbb39d4e27cd9a367f50d9acff56a
mahmoud-mhamed <mahmoud-mhamed@github.com>
2021-01-08 20:40:42 +0200

    first commit

---
+++ alertify/alertify.blade.php
@@ -0,0 +1,7 @@
+@section('style')
+    <link rel="stylesheet" href="{{asset('base/alertify/alertify.core.css')}}">
+    <link rel="stylesheet" href="{{asset('base/alertify/alertify.default.css')}}">
+@append
+@section('script_head')
+    <script data-pagespeed-no-defer src="{{asset('base/alertify/alertify.js')}}"></script>
+@append
